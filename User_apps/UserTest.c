7c07f75c16169b73e16ce040170af2305e6fe5e6
Bruno Genero <generobruno@gmail.com>
2023-06-08 16:56:50 -0300

    UserTest.c Cambios

--- User_apps/UserTest.c
+++ User_apps/UserTest.c
@@ -1,26 +1,68 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_GPIO_PINS   2
+#define MAX_GPIO_NUMBER 32
+#define BUF_SIZE        3
+#define PATH_SIZE       20
 
 int main() {
 
-	int f = open("/dev/GPIO_Driver", O_RDWR);
+	int i = 0, index = 0;
+    int fd[NUM_GPIO_PINS];
+    char path[PATH_SIZE];
+    char buf[BUF_SIZE];
+    char readBuf[2];
+	int pinIn1 = 17, pinIn2 = 22;
 
-	if(f < 0) {
-	    printf("Could not open dev file\n");
-	    return 1;
+	// Open pin GPIO 17
+	snprintf(path, sizeof(path), "/dev/raspiGpio%d", pinIn1);
+	fd[index] = open(path, O_RDWR);
+	if (fd[index] < 0) {
+		perror("Error opening GPIO pin");
+		exit(EXIT_FAILURE);
 	}
 
-	while(1)
-	{
-		write(f, "1", 2);
-		usleep(500000);
-		write(f, "0", 2);
-		usleep(500000);
+	// Open pin GPIO 22
+	snprintf(path, sizeof(path), "/dev/raspiGpio%d", pinIn2);
+	fd[index] = open(path, O_RDWR);
+	if (fd[index] < 0) {
+		perror("Error opening GPIO pin");
+		exit(EXIT_FAILURE);
 	}
 
+	// Set direction of GPIO pins to input
+    printf("Set pins to input\n");
+    strncpy(buf, "in", 2);
+    buf[2] = '\0';
 
-	return 0;
+    for (index = 0; index < NUM_GPIO_PINS; index++) {
+        if (write(fd[index], buf, sizeof(buf)) < 0) {
+            perror("write, set pin input");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+	// Read logic level of GPIO pins and display them to the terminal
+    index = 0;
+    for (i = 0; i < MAX_GPIO_NUMBER; i++) {
+        if (i != 0 && i != 1 && i != 5 && i != 6 &&
+            i != 12 && i != 13 && i != 16 && i != 19 &&
+            i != 20 && i != 21 && i != 26) {
+            if (read(fd[index], readBuf, 1) < 1) {
+                perror("write, set pin input");
+                exit(EXIT_FAILURE);
+            }
+            readBuf[1] = '\0';
+            printf("GPIO pin: %d Logic level: %s\n", i, readBuf);
+            index++;
+        }
+    }
+
+	return EXIT_SUCCESS;
 }
