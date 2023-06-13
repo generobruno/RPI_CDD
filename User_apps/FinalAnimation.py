5db79059fde64c063e6d522252b5ac82a0b708b6
Bruno Genero <generobruno@gmail.com>
2023-06-13 10:00:53 -0300

    cambios menores

--- User_apps/FinalAnimation.py
+++ User_apps/FinalAnimation.py
@@ -4,12 +4,6 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
-
-gpio_number = input("Enter the GPIO pin number: ")
-filename = f"/dev/raspiGpio{gpio_number}"
-
 def animate(i):
     with open(filename, 'r') as file:
         y_values = file.readline().strip()  # Read the line and remove leading/trailing whitespace
@@ -17,17 +11,30 @@ def animate(i):
         xs = list(range(1, len(ys) + 1))  # Generate x values
     ax1.clear()
     ax1.plot(xs, ys)
+    ax1.set_title(f"raspiGpio: {filename}")
 
-try:
-    with open(filename, 'r'):
-        pass
-except FileNotFoundError:
-    print(f"File '{filename}' not found. Please make sure the file exists.")
-    raise
+def get_filename():
+    gpio_number = input("Enter the GPIO pin number (or 'q' to quit): ")
+    if gpio_number == 'q':
+        return None
+    return f"raspiGpio{gpio_number}"
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+filename = None
 
-try:
-    plt.show()
-except KeyboardInterrupt:
-    pass
+while True:
+    if filename is None:
+        filename = get_filename()
+        if filename is None:
+            break
+    
+    try:
+        with open(filename, 'r'):
+            fig = plt.figure()
+            ax1 = fig.add_subplot(1, 1, 1)
+            ani = animation.FuncAnimation(fig, animate, interval=1000)
+            plt.show()
+            plt.close(fig)
+            filename = None
+    except FileNotFoundError:
+        print(f"File '{filename}' not found. Please make sure the file exists.")
+        filename = None
